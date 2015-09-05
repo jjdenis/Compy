d0d7122177b3c7199b10eba8d40666b05fcb46b9
Juan José Denis Corrales <jjdenis@gmail.com>
2015-09-05 21:12:53 +0200

    Añade ejemplo

---
+++ ejemplo.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from src.run import run
+import time
+
+def programa_principal(pnt):
+
+    pnt.printf(u"Hola")
+
+    for i in range(0, 100):
+        print i
+        pnt.printf(i, i, next_line=False)
+
+    pnt.printf(u"Hola", 'cyan')
+    pnt.printf()
+    pnt.printf(u"Hola", color='green')
+
+run(programa_principal)

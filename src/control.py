70880e113027e1552c49093e3fc2b71a3664dd73
casa <jjdenis@gmail.com>
2015-09-11 04:55:52 +0200

    printf se reinicia con clear screen poke None borra

--- src/control.py
+++ src/control.py
@@ -22,7 +22,7 @@ class Control(object):
         self.fm_color = INIT_FM_COLOR
         self.bg_color = INIT_BG_COLOR
         self.ch_color = INIT_CH_COLOR
-        self._write_canvas()
+        self._reset_canvas()
         self.printf(INIT_MSG)
 
     def poke(self, x, y, code, color = None):
@@ -30,7 +30,10 @@ class Control(object):
             color_c = colors.get_color(color)
             self.ch_color = color_c
 
-        char_id = char_table.get_identifier(code)
+        if code:
+            char_id = char_table.get_identifier(code)
+        else:
+            char_id = 32
         # c stands for corrected
         cx, cy = self.map.set_poked(x, y, char_id, self.ch_color)
         self._send_to_view('poke', cx, cy, char_id, self.ch_color, self.bg_color)
@@ -69,16 +72,17 @@ class Control(object):
 
     def set_bg_color(self, color):
         self.bg_color = color
-        self._write_canvas()
+        self._reset_canvas()
         self._write_all_chars()
 
     def set_fm_color(self, color):
         self.fm_color = color
-        self._write_canvas()
+        self._reset_canvas()
         self._write_all_chars()
 
     def clear_screen(self):
-        self._write_canvas()
+        self._reset_canvas()
+        self.printmap.go_end_scr()
         self.map.clear(self.bg_color)
 
     def stop(self):
@@ -93,8 +97,8 @@ class Control(object):
         key = self.key.check_for_key()
         return key
 
-    def _write_canvas(self):
-        self._send_to_view('reset_screen', self.fm_color, self.bg_color)
+    def _reset_canvas(self):
+        self._send_to_view('reset_canvas', self.fm_color, self.bg_color)
 
     def _write_all_chars(self):
         for poked in self.map.written:
